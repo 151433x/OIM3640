30be61ec2800984ce9990fdd1242e949a2439a2b
151433x <75758020+151433x@users.noreply.github.com>
2022-01-25 16:53:41 -0500

    uploaded  session3

--- session2/calc.py
+++ session2/calc.py
@@ -1,14 +1,12 @@
 # how many seconds are there in 42 minutes and 42 seconds?
 # 60 seconds in a minute
-print((42*60)+42) 
+print((42*60)+42)
 # 2562 seconds in 42 mins nd 42 secs
 
-#how many miles are there in 10 kilometeres? hint: there are 1.61 kilometeres in a mile.
-print(10/1.61)  
-#6.21 miles
-#if you run a 10 kilometer in 42 mintues and 42 seconds, what is your average pace? (time in time per miles and seconds) what is your average speed in miles per hour
+# how many miles are there in 10 kilometeres? hint: there are 1.61 kilometeres in a mile.
+print(10/1.61)
+# 6.21 miles
+# if you run a 10 kilometer in 42 mintues and 42 seconds, what is your average pace? (time in time per miles and seconds) what is your average speed in miles per hour
 
-print(2562/6.21) # seconds/ miles, 412 seconds a mile  or around 
-print(6.21/2562) # miles/seconds, .00242 miles per second 
-
-
+print(2562/6.21)  # seconds/ miles, 412 seconds a mile  or around
+print(6.21/2562)  # miles/seconds, .00242 miles per second
---
+++ session3/hello.py
@@ -0,0 +1,22 @@
+
+name = input('please enter your name:')
+print('Hello', name)
+
+a = 123
+a = '123'
+type(a)
+
+print(f'hello,{name}!')
+
+pi = 3.1415926
+print(f'Pi equals {pi:.5f}.')
+print(f'Pi equals {pi:8.5f}.')
+print(f'Pi equals {pi:8.2f}.')
+a = 2021
+print(f'{a:b}')
+print(f'{a:X}')
+print(f'{a:o}')
+
+s1='a'
+s2='ab'
+s3='abc'
