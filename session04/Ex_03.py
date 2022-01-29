6552a33e10449f11d3af19bb7c738552a6a9c94e
151433x <75758020+151433x@users.noreply.github.com>
2022-01-28 21:51:42 -0500

    removed werid variable

---
+++ session04/Ex_03.py
@@ -0,0 +1,3 @@
+#The time module provides a function, also named time, that returns the current Greenwich Mean Time in “the epoch”, which is an arbitrary time used as a reference point. On UNIX systems, the epoch is 1 January 1970.
+import time
+print(time)
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-import math
-#print(math.pi)
-age=input()
-if age>22 :
-    print('yes, you can')
-else:
-    print('no you cannot')    
