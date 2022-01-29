d6dd39f5d0c70f00128dd6fa196bce8eb40fed55
151433x <75758020+151433x@users.noreply.github.com>
2022-01-28 21:42:26 -0500

    new

---
+++ new.py
@@ -0,0 +1,7 @@
+in_china=False
+age=int(input('how old are you?'))
+if age >22 or in_china:
+    print('yes, you can')
+else:
+    print('no you cannot')
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import math
+#print(math.pi)
+age=input()
+if age>22 :
+    print('yes, you can')
+else:
+    print('no you cannot')    
