b1fbef30b8699cc0385ab6d90ef8fc4a8e5907a6
151433x <75758020+151433x@users.noreply.github.com>
2022-02-02 19:01:25 -0500

    commit

---
+++ session5/demo.py
@@ -0,0 +1,14 @@
+r1=123
+r2=567
+r3=100000000
+
+a1=3.14*r1*r1
+a2=3.14*r2*r2
+a2=3.14*r2*r2
+
+def area_of_circle(x):
+    """print the area of the circle, given the radius value"""
+    area=3.14 *x*x
+    print(area)
+
+area_of_circle(r2) # call the function
---
+++ session5/my_abs.py
@@ -0,0 +1,7 @@
+def my_abs(x):
+    if x <0:
+        print(-x)
+    else:
+        print(x)
+my_abs(3)
+my_abs(-3)
